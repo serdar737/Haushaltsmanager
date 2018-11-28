c1ce33c84cb130d9a478778e1aec5dbb7a43ebab
Kirsten <kapeine@gmx.de>
2018-11-28 12:15:48 +0100

    Änderung Kommentar

--- app/src/main/java/com/example/ks/haushaltsmanager/HaushaltsbestandActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/HaushaltsbestandActivity.java
@@ -3,6 +3,10 @@ package com.example.ks.haushaltsmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Dieser Bildschirm zeigt den Haushaltsbestand des Haushaltes an
+ */
+
 public class HaushaltsbestandActivity extends AppCompatActivity {
 
     @Override

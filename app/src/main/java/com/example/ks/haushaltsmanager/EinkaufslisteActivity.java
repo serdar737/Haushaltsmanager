2e9f34323ce895f1fdb1434e35d36e26a03bb0e7
Kirsten <kapeine@gmx.de>
2018-12-12 19:01:10 +0100

    Anfang des Artikel hinzufügens

--- app/src/main/java/com/example/ks/haushaltsmanager/EinkaufslisteActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/EinkaufslisteActivity.java
@@ -1,11 +1,14 @@
 package com.example.ks.haushaltsmanager;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +19,9 @@ public class EinkaufslisteActivity extends AppCompatActivity {
     TextView tv_benutzername, tv_haushaltsname;
     FloatingActionButton fab_artikelhinzufuegen;
     LinearLayout lleinkaufsliste;
+    Button btn_weiter;
+    EditText et_artikelname, et_menge, et_kaufhaeufigkeit;
+    String artikelname, menge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +33,32 @@ public class EinkaufslisteActivity extends AppCompatActivity {
         tv_haushaltsname = findViewById(R.id.tv_haushalteinkauf);
         fab_artikelhinzufuegen = findViewById(R.id.fab_artikelhinzufuegen);
         lleinkaufsliste = findViewById(R.id.linearlayouteinkaufsliste);
+        btn_weiter = findViewById(R.id.btn_weiter_artikel);
+        et_artikelname = findViewById(R.id.et_artikelname);
+        et_menge = findViewById(R.id.et_menge);
+        et_kaufhaeufigkeit = findViewById(R.id.et_kaufhaeufigkeit);
 
         fab_artikelhinzufuegen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                AlertDialog.Builder popupbuilder = new AlertDialog.Builder(EinkaufslisteActivity.this);
+                View popupview = getLayoutInflater().inflate(R.layout.layout_popup_artikelhinzufuegen, null);
+
+                btn_weiter.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        artikelname = et_artikelname.getText().toString();
+                        menge = et_menge.getText().toString();
+                        //TODO: CheckBoxen dem LinearLayout hinzufuegen udns chauen warum es abstuerzt
+
+                    }
+                });
+
+                popupbuilder.setView(popupview);
+                AlertDialog dialog = popupbuilder.create();
+                dialog.show();
             }
         });
     }

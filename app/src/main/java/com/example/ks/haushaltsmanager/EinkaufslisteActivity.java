5beffe8a40ab67cd238ad56c2001ebcf7f159365
Kirsten <kapeine@gmx.de>
2018-12-12 18:35:53 +0100

    Einkaufslisten Activity begonnen

--- app/src/main/java/com/example/ks/haushaltsmanager/EinkaufslisteActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/EinkaufslisteActivity.java
@@ -1,13 +1,38 @@
 package com.example.ks.haushaltsmanager;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Objects;
+
 public class EinkaufslisteActivity extends AppCompatActivity {
+
+    TextView tv_benutzername, tv_haushaltsname;
+    FloatingActionButton fab_artikelhinzufuegen;
+    LinearLayout lleinkaufsliste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_einkaufsliste);
+
+        tv_benutzername = findViewById(R.id.tv_benutzernameeinkauf);
+        tv_haushaltsname = findViewById(R.id.tv_haushalteinkauf);
+        fab_artikelhinzufuegen = findViewById(R.id.fab_artikelhinzufuegen);
+        lleinkaufsliste = findViewById(R.id.linearlayouteinkaufsliste);
+
+        fab_artikelhinzufuegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

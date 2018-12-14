3f0aa4ff635f85d5b9798787e7fa2ac6fd8d1afe
Kirsten <kapeine@gmx.de>
2018-12-14 09:37:52 +0100

    RezeptErstellenActivity bearbeitet

--- app/src/main/java/com/example/ks/haushaltsmanager/RezeptErstellenActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/RezeptErstellenActivity.java
@@ -2,12 +2,21 @@ package com.example.ks.haushaltsmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RezeptErstellenActivity extends AppCompatActivity {
+
+    EditText et_rezeptname;
+    String rezeptname;
+    Button btn_rezepthinzufuegen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rezept_erstellen);
+
+        et_rezeptname = findViewById(R.id.et_rezeptname);
+        btn_rezepthinzufuegen = findViewById(R.id.btn_rezepterstellen);
     }
 }
--- app/src/main/java/com/example/ks/haushaltsmanager/RezeptUebersichtActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/RezeptUebersichtActivity.java
@@ -1,13 +1,34 @@
 package com.example.ks.haushaltsmanager;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class RezeptUebersichtActivity extends AppCompatActivity {
+
+    LinearLayout linearlayoutrezepteuebersicht;
+    FloatingActionButton fab_neuesrezept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rezept_uebersicht);
+
+        linearlayoutrezepteuebersicht = findViewById(R.id.linearlayoutartikel);
+        fab_neuesrezept = findViewById(R.id.fab_artikelhinzufuegen);
+
+        fab_neuesrezept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(RezeptUebersichtActivity.this, RezeptErstellenActivity.class);
+                //intent.putExtra("BENUTZERNAME_UEBERGABE", benutzername);
+                startActivity(intent);
+            }
+        });
     }
 }

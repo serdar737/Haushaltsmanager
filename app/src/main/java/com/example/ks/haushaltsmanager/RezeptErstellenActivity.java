d05c835390c2f70796f07940742792711e115096
Kirsten <kapeine@gmx.de>
2018-12-14 09:58:35 +0100

    RezeptErstellenActivity bearbeitet Layout und java

--- app/src/main/java/com/example/ks/haushaltsmanager/RezeptErstellenActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/RezeptErstellenActivity.java
@@ -1,15 +1,21 @@
 package com.example.ks.haushaltsmanager;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class RezeptErstellenActivity extends AppCompatActivity {
 
     EditText et_rezeptname;
     String rezeptname;
     Button btn_rezepthinzufuegen;
+    LinearLayout linearleayoutrezepterstellen;
+    TextView zutat01, zutat02, zutat03, zutat04;
+    FloatingActionButton fab_zutathinzufuegen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,7 @@ public class RezeptErstellenActivity extends AppCompatActivity {
 
         et_rezeptname = findViewById(R.id.et_rezeptname);
         btn_rezepthinzufuegen = findViewById(R.id.btn_rezepterstellen);
+        linearleayoutrezepterstellen = findViewById(R.id.linearlayoutrezepterstellen);
+        fab_zutathinzufuegen = findViewById(R.id.fab_zutathinzufuegen);
     }
 }

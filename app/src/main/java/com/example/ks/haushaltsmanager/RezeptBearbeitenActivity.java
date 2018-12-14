dadd5e83f8504a131448d325a45492281aa8bf8a
Kirsten <kapeine@gmx.de>
2018-12-14 11:19:18 +0100

    Rezepte Erstellen udn Bearbeiten bearbeitet

---
+++ app/src/main/java/com/example/ks/haushaltsmanager/RezeptBearbeitenActivity.java
@@ -0,0 +1,82 @@
+package com.example.ks.haushaltsmanager;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+public class RezeptBearbeitenActivity extends AppCompatActivity {
+
+    EditText et_rezeptname, et_zutat;
+    String rezeptname;
+    Button btn_rezepthinzufuegen, btn_zutathinzufuegen;
+    LinearLayout linearleayoutrezepterstellen;
+    String zutat01, zutat02, zutat03, zutat04;
+    FloatingActionButton fab_zutathinzufuegen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_rezept_bearbeiten);
+
+        et_rezeptname = findViewById(R.id.et_rezeptname);
+        btn_rezepthinzufuegen = findViewById(R.id.btn_rezepterstellen);
+        linearleayoutrezepterstellen = findViewById(R.id.linearlayoutrezepterstellen);
+        fab_zutathinzufuegen = findViewById(R.id.fab_zutathinzufuegen);
+
+        fab_zutathinzufuegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder popupbuilder2 = new AlertDialog.Builder(RezeptBearbeitenActivity.this);
+                View popupviewrezepte = getLayoutInflater().inflate(R.layout.popup_zutathinzufuegen, null);
+
+                et_zutat = popupviewrezepte.findViewById(R.id.et_zutat);
+                btn_zutathinzufuegen = popupviewrezepte.findViewById(R.id.btn_zutathinzufuegenpopup);
+
+                popupbuilder2.setView(popupviewrezepte);
+                final AlertDialog dialogrezepte = popupbuilder2.create();
+
+                btn_zutathinzufuegen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        zutat01 = et_zutat.getText().toString();
+                        zutathinzufugen();
+                        dialogrezepte.hide();
+                    }
+                });
+
+                dialogrezepte.show();
+            }
+        });
+
+        btn_rezepthinzufuegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void zutathinzufugen() {
+
+        TextView zutat = new TextView(getApplicationContext());
+        zutat.setText(zutat01);
+        linearleayoutrezepterstellen.addView(zutat);
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/ks/haushaltsmanager/RezeptErstellenActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/RezeptErstellenActivity.java
@@ -1,44 +1,37 @@
 package com.example.ks.haushaltsmanager;
 
-import android.support.design.widget.FloatingActionButton;
+
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import java.util.Objects;
+
+
 
 public class RezeptErstellenActivity extends AppCompatActivity {
 
     EditText et_rezeptname;
-    String rezeptname;
-    Button btn_rezepthinzufuegen;
-    LinearLayout linearleayoutrezepterstellen;
-    TextView zutat01, zutat02, zutat03, zutat04;
-    FloatingActionButton fab_zutathinzufuegen;
+    Button btn_weiter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_rezept_erstellen);
 
-        et_rezeptname = findViewById(R.id.et_rezeptname);
-        btn_rezepthinzufuegen = findViewById(R.id.btn_rezepterstellen);
-        linearleayoutrezepterstellen = findViewById(R.id.linearlayoutrezepterstellen);
-        fab_zutathinzufuegen = findViewById(R.id.fab_zutathinzufuegen);
+        et_rezeptname = findViewById(R.id.et_rezeptbenennen);
+        btn_weiter = findViewById(R.id.btn_rezeptnameweiter);
 
-        fab_zutathinzufuegen.setOnClickListener(new View.OnClickListener() {
+        btn_weiter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
-        });
-
-        btn_rezepthinzufuegen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
+                Intent intent = new Intent(RezeptErstellenActivity.this, RezeptBearbeitenActivity.class);
+                startActivity(intent);
             }
         });
     }

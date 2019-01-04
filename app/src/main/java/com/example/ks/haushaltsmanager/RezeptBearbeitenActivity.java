baf3d0c4eb5554fe4410670050f308b15dd453c1
Kirsten <kapeine@gmx.de>
2019-01-04 15:10:00 +0100

    Am Rezepte Erstellen weitergearbeitet, bis zu 20 Zutaten können hinzugefügt werden

--- app/src/main/java/com/example/ks/haushaltsmanager/RezeptBearbeitenActivity.java
+++ app/src/main/java/com/example/ks/haushaltsmanager/RezeptBearbeitenActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -31,7 +32,7 @@ public class RezeptBearbeitenActivity extends AppCompatActivity {
     String rezeptname;
     Button btn_rezepthinzufuegen, btn_zutathinzufuegen;
     LinearLayout linearleayoutrezepterstellen;
-    String zutat01, zutat02, zutat03, zutat04, zutat05, zutat06, zutat07, zutat08, zutat09, zutat10, zutat11, zutat12, zutat13, zutat14, zutat15, zutat16, zutat17, zutat18, zutat19, zutat20;
+    String zutat01 = "-1", zutat02 = "-1", zutat03 = "-1", zutat04 = "-1", zutat05 = "-1", zutat06 = "-1", zutat07 = "-1", zutat08 = "-1", zutat09 = "-1", zutat10 = "-1", zutat11 = "-1", zutat12 = "-1", zutat13 = "-1", zutat14 = "-1", zutat15 = "-1", zutat16 = "-1", zutat17 = "-1", zutat18 = "-1", zutat19 = "-1", zutat20 = "-1";
     FloatingActionButton fab_zutathinzufuegen;
     int haushaltsid;
 
@@ -72,7 +73,6 @@ public class RezeptBearbeitenActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
 
-                        zutat01 = et_zutat.getText().toString();
                         zutathinzufugen();
                         dialogrezepte.hide();
                     }
@@ -147,7 +147,92 @@ public class RezeptBearbeitenActivity extends AppCompatActivity {
     public void zutathinzufugen() {
 
         TextView zutat = new TextView(getApplicationContext());
-        zutat.setText(zutat01);
+
+        if(zutat01.equals("-1")) {
+            zutat01 = et_zutat.getText().toString();
+            zutat.setText(zutat01);
+        }
+        else if (zutat02.equals("-1")) {
+            zutat02 = et_zutat.getText().toString();
+            zutat.setText(zutat02);
+        }
+        else if (zutat03.equals("-1")) {
+            zutat03 = et_zutat.getText().toString();
+            zutat.setText(zutat03);
+        }
+        else if (zutat04.equals("-1")) {
+            zutat04 = et_zutat.getText().toString();
+            zutat.setText(zutat04);
+        }
+        else if (zutat05.equals("-1")) {
+            zutat05 = et_zutat.getText().toString();
+            zutat.setText(zutat05);
+        }
+        else if (zutat06.equals("-1")) {
+            zutat06 = et_zutat.getText().toString();
+            zutat.setText(zutat06);
+        }
+        else if (zutat07.equals("-1")) {
+            zutat07 = et_zutat.getText().toString();
+            zutat.setText(zutat07);
+        }
+        else if (zutat08.equals("-1")) {
+            zutat08 = et_zutat.getText().toString();
+            zutat.setText(zutat08);
+        }
+        else if (zutat09.equals("-1")) {
+            zutat09 = et_zutat.getText().toString();
+            zutat.setText(zutat09);
+        }
+        else if (zutat10.equals("-1")) {
+            zutat10 = et_zutat.getText().toString();
+            zutat.setText(zutat10);
+        }
+        else if (zutat11.equals("-1")) {
+            zutat11 = et_zutat.getText().toString();
+            zutat.setText(zutat11);
+        }
+        else if (zutat12.equals("-1")) {
+            zutat12 = et_zutat.getText().toString();
+            zutat.setText(zutat12);
+        }
+        else if (zutat13.equals("-1")) {
+            zutat13 = et_zutat.getText().toString();
+            zutat.setText(zutat13);
+        }
+        else if (zutat14.equals("-1")) {
+            zutat14 = et_zutat.getText().toString();
+            zutat.setText(zutat14);
+        }
+        else if (zutat15.equals("-1")) {
+            zutat15 = et_zutat.getText().toString();
+            zutat.setText(zutat15);
+        }
+        else if (zutat16.equals("-1")) {
+            zutat16 = et_zutat.getText().toString();
+            zutat.setText(zutat16);
+        }
+        else if (zutat17.equals("-1")) {
+            zutat17 = et_zutat.getText().toString();
+            zutat.setText(zutat17);
+        }
+        else if (zutat18.equals("-1")) {
+            zutat18 = et_zutat.getText().toString();
+            zutat.setText(zutat18);
+        }
+        else if (zutat19.equals("-1")) {
+            zutat19 = et_zutat.getText().toString();
+            zutat.setText(zutat19);
+        }
+        else if (zutat20.equals("-1")) {
+            zutat20 = et_zutat.getText().toString();
+            zutat.setText(zutat20);
+        }
+        else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Du hast bereits 20 Zutaten hinzugefügt", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
         linearleayoutrezepterstellen.addView(zutat);
     }
 
